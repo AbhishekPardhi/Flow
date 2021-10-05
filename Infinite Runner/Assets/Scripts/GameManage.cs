8931f1709718172c7e06d5e353f186c474889855
Soham Samaddar <sohams20@iitk.ac.in>
2021-10-05 13:30:57 +0530

    Added pulsating effect

--- Infinite Runner/Assets/Scripts/GameManage.cs
+++ Infinite Runner/Assets/Scripts/GameManage.cs
@@ -4,4 +4,19 @@ public class GameManage : MonoBehaviour
 {
     public static float x_Velocity = 15.0f;
     public static int fixedKillDistance = -15;
+
+    private float m_Timer;
+    public static float width = 0.0f;
+
+    void Update() {
+        pulsating();
+    }
+
+    void pulsating() {
+        m_Timer += Time.deltaTime;
+        if(m_Timer > 4)
+            m_Timer = m_Timer - 4;
+        float sine = (float)(Mathf.Sin(4*m_Timer)/2);
+        width = sine*sine;
+    }
 }
