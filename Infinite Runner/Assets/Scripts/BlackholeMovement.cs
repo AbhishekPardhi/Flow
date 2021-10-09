4506c2c12adb8c670f9b293b878294a0372fa35c
Soham Samaddar <sohams20@iitk.ac.in>
2021-10-09 13:22:36 +0530

    BackupFlow advanced

---
+++ Infinite Runner/Assets/Scripts/BlackholeMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeMovement : MonoBehaviour
+{   
+    private float speed = 5.0f;
+    void Update() {
+        transform.position += Vector3.left * GameManage.x_Velocity*Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * speed, 8) - 4, 0);
+
+        if(transform.position.x < GameManage.fixedKillDistance)
+            Destroy(gameObject);
+    }
+}
