3b918e284392eb5a9c061358726668c8f79e7dc0
AbhishekPardhi <75310396+AbhishekPardhi@users.noreply.github.com>
2021-10-10 22:55:45 +0530

    mauj karo

---
+++ Infinite Runner/Assets/Scripts/Fade.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator fade;
+    public int buildindex;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FadeOut()
+    {
+        fade.SetTrigger("FadeOut");
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(buildindex);
+    }
+}
---
+++ Infinite Runner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D play;
+    public Rigidbody2D options;
+    public Rigidbody2D quit;
+    public float x=1f;
+    public float y = 1f;
+
+    void Start()
+    {
+        giverandomvel(play);
+        giverandomvel(options);
+        giverandomvel(quit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void giverandomvel(Rigidbody2D rig)
+    {
+        Vector3 vec = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        vec.Normalize();
+        float vel = Random.Range(30, x);
+        rig.velocity = vel * vec;
+        rig.angularVelocity = y * vel;
+    }
+
+
+}
