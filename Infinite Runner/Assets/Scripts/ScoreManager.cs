570b94e62800726e43428a90969b197ef6fe88ea
AbhishekPardhi <75310396+AbhishekPardhi@users.noreply.github.com>
2021-10-12 00:08:13 +0530

    .

--- Infinite Runner/Assets/Scripts/ScoreManager.cs
+++ Infinite Runner/Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Lean.Transition;
+using Lean.Common;
 
 public class ScoreManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private bool newhighscore=false;
     public int score;
+    public GameObject leanpulse;
+    
     void Start()
     {
         
@@ -18,7 +22,7 @@ public class ScoreManager : MonoBehaviour
         if (score > PlayerPrefs.GetInt("HighScore", 0) && newhighscore==false)
         {
             newhighscore = true;
-            
+            leanpulse.GetComponent<LeanPulse>().Pulse();
         }
     }
 }
