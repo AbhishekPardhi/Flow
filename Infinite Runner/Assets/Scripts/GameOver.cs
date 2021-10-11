0fe9a5987673a67ecdb6fc76b9773ebc932cda97
AbhishekPardhi <75310396+AbhishekPardhi@users.noreply.github.com>
2021-10-12 02:45:37 +0530

    .

--- Infinite Runner/Assets/Scripts/GameOver.cs
+++ Infinite Runner/Assets/Scripts/GameOver.cs
@@ -9,20 +9,33 @@ public class GameOver : MonoBehaviour
     public Fade fade;
     public Text text;
     public Text high;
-  public void RestartButton()
-  {
+    public AudioClip gameover;
+    public AudioClip noice;
+    public AudioSource source;
+    public void RestartButton()
+    {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         fade.FadeOut(1);
-  }
-   public void MainMenuButton()
-  {
+    }
+    public void MainMenuButton()
+    {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
         fade.FadeOut(0);
-  }
+    }
     private void Awake()
     {
         text.text = "SCORE : " + PlayerPrefs.GetInt("Score", 0);
-        if (PlayerPrefs.GetInt("Score", 0) == PlayerPrefs.GetInt("HighScore", 0)) high.enabled = true;
-        else high.enabled = false;
+        if (PlayerPrefs.GetInt("Score", 0) == PlayerPrefs.GetInt("HighScore", 0))
+        {
+            high.enabled = true;
+            source.clip = noice;
+            source.Play();
+        }
+        else
+        {
+            high.enabled = false;
+            source.clip = gameover;
+            source.Play();
+        }
     }
 }
