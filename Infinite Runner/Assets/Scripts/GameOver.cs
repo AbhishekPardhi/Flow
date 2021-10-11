fc059aa31ccd572ffc30c89605530cd5ba438781
AbhishekPardhi <75310396+AbhishekPardhi@users.noreply.github.com>
2021-10-12 01:33:53 +0530

    .

--- Infinite Runner/Assets/Scripts/GameOver.cs
+++ Infinite Runner/Assets/Scripts/GameOver.cs
@@ -6,12 +6,23 @@ using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
+    public Fade fade;
+    public Text text;
+    public Text high;
   public void RestartButton()
   {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        fade.FadeOut(1);
   }
    public void MainMenuButton()
   {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        fade.FadeOut(0);
   }
+    private void Awake()
+    {
+        text.text = "SCORE : " + PlayerPrefs.GetInt("Score", 0);
+        if (PlayerPrefs.GetInt("Score", 0) == PlayerPrefs.GetInt("HighScore", 0)) high.enabled = true;
+        else high.enabled = false;
+    }
 }
--- Infinite Runner/Assets/Scripts/ScoreManager.cs
+++ Infinite Runner/Assets/Scripts/ScoreManager.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using Lean.Transition;
 using Lean.Common;
+using TMPro;
 
-public class ScoreManager : MonoBehaviour
+namespace Lean.Gui
 {
-    // Start is called before the first frame update
-    private bool newhighscore=false;
-    public int score;
-    public GameObject leanpulse;
-    
-    void Start()
+    public class ScoreManager : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (score > PlayerPrefs.GetInt("HighScore", 0) && newhighscore==false)
+        // Start is called before the first frame update
+        private bool newhighscore = false;
+        public int score;
+        public float sc;
+        public int highscore;
+        public LeanPulse leanpulse;
+        public TMP_Text scoreText;
+        void Start()
         {
-            newhighscore = true;
-            //leanpulse.GetComponent<refe>
+            scoreText.text = "0";
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (score > PlayerPrefs.GetInt("HighScore", 0) && newhighscore == false)
+            {
+                newhighscore = true;
+                leanpulse.Pulse();
+            }
+            highscore = PlayerPrefs.GetInt("HighScore", 0);
+            score = Mathf.FloorToInt(sc);
+            scoreText.text = "" + score;
+            if (!GameManage.playerDeath) sc += Time.deltaTime * 2;
+            else
+            {
+                PlayerPrefs.SetInt("Score", score);
+                if(score > PlayerPrefs.GetInt("HighScore", 0)) PlayerPrefs.SetInt("HighScore", score);
+            }
         }
     }
 }
