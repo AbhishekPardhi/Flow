e581b2de551fe2381c60681ebbc24770f84233de
AbhishekPardhi <75310396+AbhishekPardhi@users.noreply.github.com>
2021-10-11 18:51:25 +0530

    .

--- Infinite Runner/Assets/Scripts/IGOptions.cs
+++ Infinite Runner/Assets/Scripts/IGOptions.cs
@@ -8,9 +8,13 @@ public class IGOptions : MonoBehaviour
     // Start is called before the first frame update
     public Fade fade;
     public AudioMixer audioMixer;
+    public AudioSource asource;
+    public AudioClip t1;
+    public AudioClip t2;
     void Start()
     {
         fade = FindObjectOfType<Fade>();
+        audioMixer.SetFloat("Volume", PlayerPrefs.GetFloat("Volume",0));
     }
 
     // Update is called once per frame
@@ -37,5 +41,15 @@ public class IGOptions : MonoBehaviour
     public void TurnOffVolume()
     {
         audioMixer.SetFloat("Volume", -80);
+    }
+    private void changeToT1()
+    {
+        asource.clip = t1;
+        Invoke(nameof(changeToT2), asource.clip.length);
+    }
+    private void changeToT2()
+    {
+        asource.clip = t2;
+        Invoke(nameof(changeToT1), asource.clip.length);
     }
 }
